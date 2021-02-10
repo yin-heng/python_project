22efd31e0e049c24602219f11026c00e5378ae28
Heng <hengyin4@gmail.com>
2021-02-09 23:59:31 -0800

    figure out how setUp and tearDown works in python unittest

--- hw1/hw1_unit_test.py
+++ hw1/hw1_unit_test.py
@@ -1,38 +1,33 @@
 import unittest
 import HW1
 
-operations = None
-
 class TestHW1(unittest.TestCase):
 
     def setUp(self):
-        operations = HW1.Operations()
+        self.operations = HW1.Operations()
 
     def tearDown(self):
-        operations = None
+        self.operations = None
 
     def test_1(self):
-        operations = HW1.Operations()
-        operations.write('Hello world')
-        operations.read()
-        operations.status()
-        operations.write_to_offset(int(11),'! This is a test')
-        self.assertEqual(operations.read().replace(b'\x00', b'').decode('utf-8'), 'Hello world! This is a test')
+        self.operations.write('Hello world')
+        self.operations.read()
+        self.operations.status()
+        self.operations.write_to_offset(int(11),'! This is a test')
+        self.assertEqual(self.operations.read().replace(b'\x00', b'').decode('utf-8'), 'Hello world! This is a test')
 
     def test_2(self):
-        operations = HW1.Operations()
-        operations.write('Today has been a bland day')
-        operations.read()
-        operations.status()
-        operations.write_to_offset(int(17),'great')
-        self.assertEqual(operations.read().replace(b'\x00', b'').decode('utf-8'), 'Today has been a great day')
+        self.operations.write('Today has been a bland day')
+        self.operations.read()
+        self.operations.status()
+        self.operations.write_to_offset(int(17),'great')
+        self.assertEqual(self.operations.read().replace(b'\x00', b'').decode('utf-8'), 'Today has been a great day')
 
     def test_exception(self):
-        operations = HW1.Operations()
-        operations.write('Hello world')
-        operations.read()
-        operations.status()
-        self.assertRaises(Exception, "offset to write is invalid", operations.write_to_offset, int(17),'! This is a test')
+        self.operations.write('Hello world')
+        self.operations.read()
+        self.operations.status()
+        self.assertRaises(Exception, "offset to write is invalid", self.operations.write_to_offset, int(17),'! This is a test')
 
 if __name__ == '__main__':
     unittest.main()
